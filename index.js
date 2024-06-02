add7c4e5a5fc4614ce8b2df077bb9d208620d486
finderfail <shadowmaster1957@outlook.com>
2024-06-02 14:12:06 +0300

    yep

--- index.js
+++ index.js
@@ -2,24 +2,12 @@ const { encrypt, decrypt } = require("./crypto");
 // Copyright Ilia Finderov 2024 at Kriloud
 
 console.log("Welcome to simple coder and encoder cli app\n This app using Kriloud crypto library.\n");
-console.log();
-
 prompt('First you need to choose "Decrypt" or "Encrypt": ', function (answ) {
-    if (answ == "Decrypt") {
+    if (answ == "decrypt") {
         prompt('You choose Decrypt so type crypted thing: ', function (xsr){
             console.log(decrypt(xsr));
             process.exit();
         });
-    } else if (answ == "decrypt") {
-        prompt('You choose Decrypt so type crypted thing: ', function (xsr){
-            console.log(decrypt(xsr));
-            process.exit();
-        });
-    } else if (answ == "Encrypt") {
-        prompt('You choose Encrypt so type anything: ', function (xsr){
-            console.log(encrypt(xsr));
-            process.exit();
-        }); 
     } else if (answ == "encrypt") {
         prompt('You choose Encrypt so type anything: ', function (xsr){
             console.log(encrypt(xsr));
@@ -28,11 +16,6 @@ prompt('First you need to choose "Decrypt" or "Encrypt": ', function (answ) {
     } else {console.log("You typed something strange.");}
     
 });
-
-
-
-
-
 function prompt(question, callback) {
     var stdin = process.stdin,
         stdout = process.stdout;
